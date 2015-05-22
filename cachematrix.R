977a801ace98f7dfba9ed26b3a2bc5879bb21b1e
smurthy710 <sashi.murthy@asml.com>
2015-05-22 13:02:13 -0700

    Homework assigment file
    This file is the code for the homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is desgined to cache the matrix inverse for future use
+## functions makeCacheMatrix does the cache and the function cacheSolve returns the inversed matrix
 
-## Write a short comment describing this function
+## In the vaiable x is set to orginal matrix and the m is intially set to Null then to inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setinv <- function(solve) m <<- solve
+     getinv <- function() m
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Checks if the inverse exists - it will return the inverse if it exists else calculates the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+           m <- x$getinv()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data)
+     x$setinv(m)
+     m
 }
